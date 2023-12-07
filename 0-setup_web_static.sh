8cc56065c8eb4eeae95671d30e127cc733e3c878
Aymen Idouahman <aymanidou@gmail.com>
2023-12-07 22:07:03 +0100

    fix serving source uri

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -21,9 +21,9 @@ echo "$home_page" > /data/web_static/releases/test/index.html
 [ -d /data/web_static/current ] && rm -rf /data/web_static/current
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 # give ownership to ubuntu user
-chown -R ubuntu:ubuntu /data
+chown -R root:root /data
 
-mystr="\n\tlocation \/hbnb_static\/ \{\n\t\t# redirection to the current symbolic link\n\t\talias \/data\/web_static\/current\;\n\t\ttry_files \$uri \$uri\/ =404\;\n\t\}"
+mystr="\n\tlocation = \/hbnb_static \{\n\t\talias \/data\/web_static\/current\/\;\n\t\ttry_files \$uri \$uri\/ =404\;\n\t\}"
 replace="server {"
 sed -i "0,/$replace/{s/$replace/$replace$mystr/}" /etc/nginx/sites-available/default
 # override the link to default
