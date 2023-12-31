7fd37f58bfb317c8868bbe73d723b3cb9dae1d9b
Aymen Idouahman <aymanidou@gmail.com>
2023-12-31 23:19:00 +0100

    8. List of states

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''8. List of states'''
+from models import storage, State
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(e):
+    '''close storage'''
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    '''fetch all states'''
+    states = storage.all(State).values()
+    return render_template("7-states_list.html", states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
