0013d54a31e74920882b2c753408ac1d2120f3f8
Aymen Idouahman <aymanidou@gmail.com>
2024-01-01 17:27:06 +0100

    10. States and State

---
+++ web_flask/9-states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""8. List of states"""
+from models import storage, State, City
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(e):
+    """close storage"""
+    storage.close()
+
+
+@app.route("/states", strict_slashes=False)
+def display_states():
+    """fetch all states"""
+    states = storage.all(State).values()
+    return render_template("9-states.html", states=states, cities=None)
+
+@app.route("/states/<id>", strict_slashes=False)
+def display_states(id):
+    """fetch all states"""
+    state_id = "State." + id
+    states = storage.all(State)
+    if (state_id in states):
+        states = states[state_id]
+    return render_template("9-states.html", states=states, cities="show")
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
