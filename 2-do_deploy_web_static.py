e8750e7464728f1b04df7de30d1b7aae61af668d
Aymen Idouahman <aymanidou@gmail.com>
2024-03-08 22:16:58 +0100

    2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,10 +5,9 @@ import os
 from fabric.operations import local, put, run
 from fabric.api import runs_once, local, env
 
-env.hosts = ["54.236.49.164", "52.86.203.42"]
+env.hosts = ["54.167.96.145", "34.239.254.105"]
 
 
-@runs_once
 def do_pack():
     """this function pack our site in a tgz file"""
     if (not os.path.exists("versions")):
@@ -33,19 +32,20 @@ def do_deploy(archive_path):
     """this function deploy our website to the servers"""
     if (not os.path.exists(archive_path)):
         return False
+
     file_name = os.path.basename(archive_path)
     folder_name = file_name.replace('.tgz', '')
     extraction_path = "/data/web_static/releases/{}".format(folder_name)
     print(file_name)
     try:
-        put(archive_path, "/tmp/{}".format(file_name))
+        put(archive_path, "/tmp/")
         run("mkdir -p {}".format(extraction_path))
         run("tar -xzf /tmp/{} -C {}".format(file_name, extraction_path))
         run("rm -rf /tmp/{}".format(file_name))
         run("mv {}/web_static/* {}/ ".format(extraction_path, extraction_path))
-        run("rm -rf {}/web_static/ ".format(extraction_path))
+        run("rm -rf {}/web_static ".format(extraction_path))
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(extraction_path))
+        run("ln -s {}/ /data/web_static/current".format(extraction_path))
         print('New version deployed!')
         return True
     except Exception:
